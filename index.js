58073c83c93dbd60ac769c92464a3f9e7d3a75e8
Karalyn Ferrari <lifesmygift@gmail.com>
2018-02-21 21:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ function subtract(a,b) {
 function multiply(a,b) {
   return a*b
 }
+
+/*divide, divides to floats and returns the answer
+*/
+function divide(a,b) {
+  return a/b
+}
+
+/* inc, increments a float n by 1 and returns the result
+*/
+function inc(n) {
+  return n++
+}
+
+/*dec, decrements a float n and returns the result
+*/
+function dec(n) {
+  return n--
+}
+
+/* makeInt, accepts a string and converts into an integer
+*/
+function makeInt(n) {
+  return parseInt(n,10)
+}
+
+/*preserveDecimal(n) accepts a string and turns it into a float*/
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
